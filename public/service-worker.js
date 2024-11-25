008f3e6806bdccb4c6cf709eef103cbcfbe9db89
kunseal <kundan.seal@publicissapient.com>
2024-11-25 10:20:45 +0530

    Pausing service Worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,52 +1,52 @@
 /* eslint-disable no-restricted-globals */
-const CACHE_NAME = "weather-app-cache-v1";
-const PROD_URLS_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/static/js/bundle.js",
-  "/manifest.json",
-  "/static/media/add_location.3a13adc98209014ee677b2933e4fd4e4.svg",
-  "/static/media/not_found.f7a1746b6c32d82a3f32c77fc778e768.svg",
-  "/logo192.png",
-  "/favicon.ico",
-  "/static/css/main.cd35a900.css",
-  "/static/css/main.cd35a900.css.map",
-  "/static/js/453.d1bcbf08.chunk.js",
-  "/static/js/453.d1bcbf08.chunk.js",
-  "/static/js/main.6a073244.js",
-  "/static/js/main.6a073244.js.map",
-  "/api/weather",
-];
+// const CACHE_NAME = "weather-app-cache-v1";
+// const PROD_URLS_TO_CACHE = [
+//   "/",
+//   "/index.html",
+//   "/static/js/bundle.js",
+//   "/manifest.json",
+//   "/static/media/add_location.3a13adc98209014ee677b2933e4fd4e4.svg",
+//   "/static/media/not_found.f7a1746b6c32d82a3f32c77fc778e768.svg",
+//   "/logo192.png",
+//   "/favicon.ico",
+//   "/static/css/main.cd35a900.css",
+//   "/static/css/main.cd35a900.css.map",
+//   "/static/js/453.d1bcbf08.chunk.js",
+//   "/static/js/453.d1bcbf08.chunk.js",
+//   "/static/js/main.6a073244.js",
+//   "/static/js/main.6a073244.js.map",
+//   "/api/weather",
+// ];
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(PROD_URLS_TO_CACHE);
-    })
-  );
-});
-self.addEventListener("activate", (event) => {
-  self.clients.claim(); // Claim all active clients immediately
-  console.log("Service Worker activated");
-});
+// self.addEventListener("install", (event) => {
+//   event.waitUntil(
+//     caches.open(CACHE_NAME).then((cache) => {
+//       return cache.addAll(PROD_URLS_TO_CACHE);
+//     })
+//   );
+// });
+// self.addEventListener("activate", (event) => {
+//   self.clients.claim(); // Claim all active clients immediately
+//   console.log("Service Worker activated");
+// });
 
-self.addEventListener("fetch", (event) => {
-  if (!navigator.onLine) {
-    if (event.request.url === "http://localhost:3000/static/js/main.chunk.js") {
-      event.waitUntil(
-        this.registration.showNotification("Internet", {
-          body: "internet not working",
-        })
-      );
-    }
-    event.respondWith(
-      caches.match(event.request).then((resp) => {
-        if (resp) {
-          return resp;
-        }
-        let requestUrl = event.request.clone();
-        fetch(requestUrl);
-      })
-    );
-  }
-});
+// self.addEventListener("fetch", (event) => {
+//   if (!navigator.onLine) {
+//     if (event.request.url === "http://localhost:3000/static/js/main.chunk.js") {
+//       event.waitUntil(
+//         this.registration.showNotification("Internet", {
+//           body: "internet not working",
+//         })
+//       );
+//     }
+//     event.respondWith(
+//       caches.match(event.request).then((resp) => {
+//         if (resp) {
+//           return resp;
+//         }
+//         let requestUrl = event.request.clone();
+//         fetch(requestUrl);
+//       })
+//     );
+//   }
+// });
--- src/index.tsx
+++ src/index.tsx
@@ -7,19 +7,19 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.getRegistrations().then((registrations) => {
-    registrations.forEach((registration) => registration.unregister());
-  });
-  navigator.serviceWorker
-    .register("/service-worker.js")
-    .then((registration) => {
-      console.log("Service Worker registered with scope:", registration.scope);
-    })
-    .catch((error) => {
-      console.error("Service Worker registration failed:", error);
-    });
-}
+// if ("serviceWorker" in navigator) {
+//   navigator.serviceWorker.getRegistrations().then((registrations) => {
+//     registrations.forEach((registration) => registration.unregister());
+//   });
+//   navigator.serviceWorker
+//     .register("/service-worker.js")
+//     .then((registration) => {
+//       console.log("Service Worker registered with scope:", registration.scope);
+//     })
+//     .catch((error) => {
+//       console.error("Service Worker registration failed:", error);
+//     });
+// }
 
 root.render(<App />);
 
