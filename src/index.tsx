35dffaf14c50b12f036100c4197fa7b68d90feb0
kunseal <kundan.seal@publicissapient.com>
2024-11-25 09:41:21 +0530

    Updated service worker registration

--- src/index.tsx
+++ src/index.tsx
@@ -8,17 +8,19 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/service-worker.js")
-      .then((registration) => {
-        console.log("Service Worker registered: ");
-      })
-      .catch((error) => {
-        console.error("Service Worker registration failed:", error);
-      });
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    registrations.forEach((registration) => registration.unregister());
   });
+  navigator.serviceWorker
+    .register("/service-worker.js")
+    .then((registration) => {
+      console.log("Service Worker registered with scope:", registration.scope);
+    })
+    .catch((error) => {
+      console.error("Service Worker registration failed:", error);
+    });
 }
+
 root.render(<App />);
 
 reportWebVitals();
